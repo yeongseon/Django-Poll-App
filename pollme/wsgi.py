2d04a901a25538c3008cd024575a17955657eda2
Yeongseon Choe <yeongseon.choe@gmail.com>
2022-07-11 20:29:51 +0900

    Update wsgi.py

--- pollme/wsgi.py
+++ pollme/wsgi.py
@@ -11,6 +11,9 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pollme.settings')
+# Check for the WEBSITE_HOSTNAME environment variable to see if we are running in Azure Ap Service
+# If so, then load the settings from production.py
+settings_module = 'pollme.production' if 'WEBSITE_HOSTNAME' in os.environ else 'pollme.settings'
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_wsgi_application()
